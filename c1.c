6a40e71d4bc0d09f1565cf886f5b179a1070a27f
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-03-02 20:00:06 +0600

    Update c1.c

--- c1.c
+++ c1.c
@@ -23,11 +23,32 @@ printf("\n");
 
 
 
+    /*1
+    2 2
+    3 3 3*/
+
+int n,row,col;
+printf("Enter N = ");
+scanf("%d",&n);
+
+for(row = 1; row<=n; row++){
+
+for(col = 1; col<=row; col++){
+
+    printf("%d ",row);
+}
+
+printf("\n");
+}
 
 
+
+
+    
 
 
 
 
     return 0;
 }
+
