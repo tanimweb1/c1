0932ef9769fa4340a7700bca4b5447b42316b496
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-03-20 01:11:12 +0600

    Refactor c1.c to include multiple patterns and checks

--- c1.c
+++ c1.c
@@ -186,6 +186,38 @@ printf("\n");
 }
 
 
+
+
+
+
+
+
+
+//2. Right Triangle 
+int n,row,col;
+scanf("%d",&n);
+
+for(row = 1; row<=n;row++){
+
+for(col= 1; col<=row;col++){
+    printf("*");
+}
+printf("\n");
+
+}
+
+
+
+
+
+
+
+
+
+
+  
+
+
     
     
 
