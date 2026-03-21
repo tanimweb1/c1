9f98a6328bf09dbfb3c8285b9ac84a2d65edf4eb
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-03-21 23:58:19 +0600

    Update c1.c

--- c1.c
+++ c1.c
@@ -5,19 +5,6 @@ int main(){
     1 2
     1 2 3*/
 
-int n,row,col;
-printf("Enter N = ");
-scanf("%d",&n);
-
-for(row = 1; row<=n; row++){
-
-for(col = 1; col<=row; col++){
-
-    printf("%d ",col);
-}
-
-printf("\n");
-}
 
 
 
